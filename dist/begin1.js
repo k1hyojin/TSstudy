5a9e90f0db400bf9603c8a1dde161e09d4ef3e8c
k1hyojin <rnjsgywls96@hanmail.net>
2022-11-28 21:58:46 +0900

    type script start

---
+++ dist/begin1.js
@@ -0,0 +1,11 @@
+"use strict";
+// age : number    // age : number 를 필수요소로 지 않으려면 age ?: number 처럼 물음표 넣어주면 된다.
+const nico = {
+    name: "nico"
+};
+const lynn = {
+    name: "lynn",
+    age: 12
+};
+// if(player.age && player.age < 10){  // player.age가 undefined 일수도 있기에 player.age 가 존재하는지 여부판단 , 10 이하인지 판단
+// }
